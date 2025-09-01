fbc69a479ae843f74a4bc990a9e331c84b08e35e
arunkumar.r10D <arunkumar.r@10decoders.in>
2025-09-01 14:36:27 +0530

    initial commit

---
+++ src/main/java/com/example/springPractice/SpringPracticeApplication.java
@@ -0,0 +1,14 @@
+package com.example.springPractice;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class SpringPracticeApplication {
+
+	public static void main(String[] args) {
+//		SpringApplication.run(SpringPracticeApplication.class, args);
+        System.out.println("Helo world");
+	}
+
+}
